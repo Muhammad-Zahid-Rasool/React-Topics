e9a0406853ce5552b10cdf2f2491d0526e7ca36b
Muhammad-Zahid-Rasool <AzooCoder@yahoo.com>
2023-01-28 22:12:15 +0500

    "Form"

---
+++ src/FormControlledComponent.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+
+export default function FormControlledComponent() {
+  
+    const {name , setName} = useState('');
+   
+    return (
+    <div>
+    <h4>Form Controled Component Exaample</h4>
+    <form>
+     <label >Name:</label> <input type="text" value={name} /><br/>
+
+    
+    </form>    
+    </div>
+  )
+}
